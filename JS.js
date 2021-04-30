950c4c861ddfb873bb8f6c9c0c771839dc7fbb6c
kseniyachernukha <78462089+Kseniya3006@users.noreply.github.com>
2021-04-30 23:46:09 +0600

    Add files via upload

--- JS.js
+++ JS.js
@@ -1,7 +1,20 @@
+
 window.onload = function(){
-   window.setInterval(
-	function(){
-	    let date = new Date();
-	    document.getElementById("clock").innerHTML = date.toLocaleTimeString();
-	}, 1000);
-}
+window.setInterval(
+function(){
+let t = new Date();
+document.getElementById("clock").innerHTML = t.toLocaleTimeString();}, 1000);}
+
+function slidecontainer(){
+	var slider = document.getElementById("myRange");
+	var output = document.getElementById("demo");
+	output.innerHTML = slider.value; 
+	slider.oninput = function() {
+    output.innerHTML = this.value;}
+	}
+
+function myFunction() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+	}
